1f566c73b069163d52bd72d2d40582e5a7b8db84
Alyona <alena.bentley.aston@gmail.com>
2025-05-14 00:44:19 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export function info() {
+  const m = new Map();
+  m.set('name', 'ajs');
+  m.set('version', '1.0.0');
+  return m;
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'dist/index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ajs', 
+    libraryTarget: 'umd', 
+    globalObject: 'this', 
+  },
+  mode: 'production', 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
